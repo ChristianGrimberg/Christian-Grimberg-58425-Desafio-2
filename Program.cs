d04c90a4127c799e87f394dd2e24075edd9f1fb3
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-29 22:56:43 +0000

    :computer: Se crea el gestor de base de datos en nueva clase.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- Program.cs
+++ Program.cs
@@ -1,35 +1,9 @@
-﻿using System.Data.SqlClient;
-
-namespace Desafio_2;
+﻿namespace Christian_Grimberg_58425_Desafio_2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string connectionString = "Server=.; Database=Desafio2; User=sa; Password=P@ssw0rd;";
-
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            string query = @"SELECT [name]
-                            FROM [sys].[databases]
-                            WHERE [name] = 'Desafio2';";
-
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                connection.Open();
-
-                using (SqlDataReader dataReader = command.ExecuteReader())
-                {
-                    if (dataReader.HasRows)
-                    {
-                        while (dataReader.Read())
-                        {
-                            var data = dataReader.GetString(0);
-                            Console.WriteLine(data);
-                        }
-                    }
-                }
-            }
-        }
+        GestorBaseDatos gestor = new GestorBaseDatos("Prueba");
     }
 }
---
+++ database/GestorBaseDatos.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+
+namespace Christian_Grimberg_58425_Desafio_2;
+
+public class GestorBaseDatos
+{
+    private readonly string _connectionString;
+    public GestorBaseDatos(string baseDeDatos)
+    {
+        this._connectionString = $"Server=.; User=sa; Password=P@ssw0rd;";
+        string newDatabaseQuery = $@"
+                                SET NOCOUNT OFF;
+
+                                IF NOT EXISTS(SELECT [name] FROM [sys].[databases] WHERE [name] = '{baseDeDatos}')
+                                BEGIN
+                                    CREATE DATABASE [{baseDeDatos}];
+                                    SELECT 'Se crea la base de datos';
+                                END
+                                ELSE
+                                BEGIN
+                                    SELECT 'La base de datos ya existe';
+                                END
+                                ";
+
+        using (SqlConnection connection = new SqlConnection(this._connectionString))
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(newDatabaseQuery, connection);
+            
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0}", reader[0]));
+                }
+            }
+
+            connection.Close();
+        }
+    }
+}
