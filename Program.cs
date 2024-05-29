c79f8dfa2c00c9ab90ef01be70388de059d5e352
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-29 22:01:51 +0000

    :monkey_face: Se agrega como ejemplo la conexion y una lectura a la base de datos.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- Program.cs
+++ Program.cs
@@ -1,7 +1,35 @@
-﻿namespace Desafio_2;
+﻿using System.Data.SqlClient;
+
+namespace Desafio_2;
 
 class Program
 {
     static void Main(string[] args)
-    {}
+    {
+        string connectionString = "Server=.; Database=Desafio2; User=sa; Password=P@ssw0rd;";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = @"SELECT [name]
+                            FROM [sys].[databases]
+                            WHERE [name] = 'Desafio2';";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    if (dataReader.HasRows)
+                    {
+                        while (dataReader.Read())
+                        {
+                            var data = dataReader.GetString(0);
+                            Console.WriteLine(data);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
