c0124471296fc6600071682165f1ffb49482a3b6
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-30 00:10:57 +0000

    :computer: Se crea metodo privado para obtener las consultas desde la base de datos.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- database/GestorBaseDatos.cs
+++ database/GestorBaseDatos.cs
@@ -5,39 +5,44 @@ namespace Christian_Grimberg_58425_Desafio_2;
 internal class GestorBaseDatos
 {
     private readonly string _connectionString;
+    private readonly SqlConnection _connection;    
     
     internal GestorBaseDatos(string server, string baseDeDatos, string user, string password)
     {
-        this._connectionString = $"Server={server}; User={user}; Password={password};";
+        _connectionString = $"Server={server}; User={user}; Password={password};";
         string newDatabaseQuery = $@"
-                                SET NOCOUNT OFF;
+                                    SET NOCOUNT OFF;
 
-                                IF NOT EXISTS(SELECT [name] FROM [sys].[databases] WHERE [name] = '{baseDeDatos}')
-                                BEGIN
-                                    CREATE DATABASE [{baseDeDatos}];
-                                    SELECT 'Se crea la base de datos [{baseDeDatos}]';
-                                END
-                                ELSE
-                                BEGIN
-                                    SELECT 'La base de datos [{baseDeDatos}] ya existe';
-                                END
-                                ";
+                                    IF NOT EXISTS(SELECT [name] FROM [sys].[databases] WHERE [name] = '{baseDeDatos}')
+                                    BEGIN
+                                        CREATE DATABASE [{baseDeDatos}];
+                                        SELECT '[INFO]: Conectado a la nueva base de datos [{baseDeDatos}]';
+                                    END
+                                    ELSE
+                                    BEGIN
+                                        SELECT '[INFO]: Conectado a la base de datos existente [{baseDeDatos}]';
+                                    END
+                                    ";
 
-        using (SqlConnection connection = new SqlConnection(this._connectionString))
+        _connection = new SqlConnection(_connectionString);
+
+        GetDatabaseQuery(_connection, newDatabaseQuery);
+    }
+
+    private static void GetDatabaseQuery(SqlConnection connection, string sqlCommand)
+    {
+        connection.Open();
+
+        SqlCommand command = new SqlCommand(sqlCommand, connection);
+        
+        using (SqlDataReader reader = command.ExecuteReader())
         {
-            connection.Open();
-
-            SqlCommand command = new SqlCommand(newDatabaseQuery, connection);
-            
-            using (SqlDataReader reader = command.ExecuteReader())
+            while (reader.Read())
             {
-                while (reader.Read())
-                {
-                    Console.WriteLine(String.Format("{0}", reader[0]));
-                }
+                Console.WriteLine(string.Format("{0}", reader[0]));
             }
-
-            connection.Close();
         }
+
+        connection.Close();
     }
 }
