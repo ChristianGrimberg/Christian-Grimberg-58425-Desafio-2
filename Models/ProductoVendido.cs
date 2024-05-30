51c0e9670653d08cc595a228154bff5030cc189c
Christian Grimberg <christian.grimberg@outlook.com>
2024-05-30 01:10:05 +0000

    :+1: Se cargan las clases del primer desafio.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ Models/ProductoVendido.cs
@@ -0,0 +1,87 @@
+﻿using System.Reflection;
+
+namespace Christian_Grimberg_58425_Desafio_2;
+
+public class ProductoVendido
+{
+	private int id;
+	public int Id
+	{
+		get
+		{
+			if(id.GetType() != typeof(int)) id = 0;
+			return id;
+		}
+		set
+		{
+			if(value.GetType() == typeof(int)) id = value;
+			else id = 0;
+		}
+	}
+
+	private int idProducto;
+	public int IdProducto
+	{
+		get
+		{
+			if(idProducto.GetType() != typeof(int)) idProducto = 0;
+			return idProducto;
+		}
+		set
+		{
+			if(value.GetType() == typeof(int)) idProducto = value;
+			else idProducto = 0;
+		}
+	}
+
+	private int stock;
+	public int Stock
+	{
+		get
+		{
+			if(stock.GetType() != typeof(int)) stock = 0;
+			return stock;
+		}
+		set
+		{
+			if(value.GetType() == typeof(int)) stock = value;
+			else stock = 0;
+		}
+	}
+
+	private int idVenta;
+	public int IdVenta
+	{
+		get
+		{
+			if(idVenta.GetType() != typeof(int)) idVenta = 0;
+			return idVenta;
+		}
+		set
+		{
+			if(value.GetType() == typeof(int)) idVenta = value;
+			else idVenta = 0;
+		}
+	}
+
+	public ProductoVendido() {}
+
+	public ProductoVendido(int _id, int _idProducto, int _stock, int _idVenta)
+	{
+		this.Id = _id;
+		this.IdProducto = _idProducto;
+		this.Stock = _stock;
+		this.idVenta = _idVenta;
+	}
+
+	public override string ToString()
+	{
+		string returnedValue = string.Empty;
+
+		foreach(PropertyInfo propertyInfo in this.GetType().GetProperties())
+		{
+			returnedValue += string.Format("{0} de {1} es: {2}\n", propertyInfo.Name, this.GetType().Name ,propertyInfo.GetValue(this));
+		}
+		return returnedValue;
+	}
+}
